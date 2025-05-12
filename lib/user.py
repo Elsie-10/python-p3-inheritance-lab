4049081d4d2f085d6cc2effb7dd5b9730e8a13d1
Elsie <elsieoscar06@gmail.com>
2025-05-12 19:40:34 +0300

    inheritance-lab

--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
 
 class User:
-    pass
+    # a constructor in python must be named __init__
+    def __init__(self,first_name,last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+my_user= User("my","User")
+print(my_user.first_name)
+print(my_user.last_name)
